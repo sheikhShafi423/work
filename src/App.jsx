45eb4777f27df62b887e760731f8f7bbe6652f00
sheikhShafi423 <sheikhshafi0912@gmail.com>
2026-03-10 21:27:04 +0600

    home add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './Component/Auth/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +7,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Home />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-blue-500">
+          Tailwind Working
+        </h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite a
-      </p>
+      
     </>
   )
 }
